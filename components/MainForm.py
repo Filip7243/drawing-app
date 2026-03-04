52130e10ca114b885a5f8e37df9aec3d283e0df0
filip7243 <filip7243@gmail.com>
2026-03-04 21:28:28 +0100

    usuniecie trybu

--- components/MainForm.py
+++ components/MainForm.py
@@ -113,10 +113,6 @@ class MainForm(QWidget):
 
         layout.addWidget(self.examine_reason, 5, 0, 1, 2)
 
-        self.mode_radios = StyledCheckBox("Tryb testu", ['Normalny', 'Uproszczony'], is_radio=True, required=True)
-
-        layout.addWidget(self.mode_radios, 6, 0, 1, 2, alignment=Qt.AlignmentFlag.AlignVCenter)
-
         button_layout = QHBoxLayout()
         button_layout.addStretch(1)  # pushuje przyciski w prawo
 
@@ -170,7 +166,7 @@ class MainForm(QWidget):
 
     def on_start_btn_click(self):
         fields = [self.first_name, self.last_name, self.date_of_birth, self.gender_radios,
-                  self.eyes_radios, self.hands_radios, self.education_dropdown, self.details_dropdown, self.mode_radios]
+                  self.eyes_radios, self.hands_radios, self.education_dropdown, self.details_dropdown]
         invalid_fields = [f for f in fields if not f.is_valid()]
 
         if invalid_fields:
